463db2d91a792d25b3ad9d1b9a840e50c64fec26
Kyle Weisse <53319891+glitterspin@users.noreply.github.com>
2021-07-28 13:37:27 -0400

    most recent changes as of 7-28-21

--- Model01-Firmware-custom1.ino
+++ Model01-Firmware-custom1.ino
@@ -174,18 +174,18 @@ KEYMAPS(
 
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
-  (Key_Backtick, Key_1, Key_2, Key_3, Key_4, Key_5, Key_CapsLock,
-   Key_Tab,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Spacebar,
+  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-    Key_LeftControl, Key_Backspace, Key_LeftShift, Key_LeftAlt, 
+   Key_LeftControl, Key_Backspace, Key_LeftShift, Key_LeftGui, 
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),          Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_LEDEffectNext,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                           Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,          Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-          Key_LeftGui, Key_RightShift, Key_Spacebar, Key_Enter,
+          Key_LeftAlt, Key_RightShift, Key_Spacebar, Key_Enter,
    ShiftToLayer(FUNCTION)),
 
 #elif defined (PRIMARY_KEYMAP_CUSTOM)
@@ -195,13 +195,13 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftShift, Key_LeftAlt,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),          Key_6, Key_7,     Key_8,         Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_LEDEffectNext,     Key_Y, Key_U,     Key_I,         Key_O,         Key_P,         Key_Equals,
-                          Key_H, Key_J,     Key_K,         Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,          Key_N, Key_M,     Key_Comma,     Key_Period,    Key_Slash,     Key_Minus,
+   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   Key_LEDEffectNext,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_Enter,
    ShiftToLayer(FUNCTION)),
 
@@ -229,18 +229,18 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (Key_LEDEffectNext, Key_F1,           Key_F2,      Key_F3,           Key_F4,        Key_F5,          ___,
-   Key_Tab,           ___,              Key_mouseUp, Key_mouseWarpEnd, Key_mouseBtnR, Key_mouseWarpNW, Key_mouseWarpNE,
-   Key_Home,          Key_mouseL,       Key_mouseDn, Key_mouseR,       Key_mouseBtnL, Key_mouseWarpW,
-   Key_End,           Key_PrintScreen,  Key_Insert,  ___,              Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
+   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_Tab,
+   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
    ___, Key_Delete, ___, ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_Home,               Key_LeftArrow,            Key_DownArrow,            Key_UpArrow,     Key_RightArrow,   Key_End,
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
    Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, ___, ___,
+   ___, ___, Key_Enter, ___,
    ___)
 ) // KEYMAPS(
 
@@ -528,11 +528,6 @@ void setup() {
   // maps for. To make things simple, we set it to five layers, which is how
   // many editable layers we have (see above).
   ColormapEffect.max_layers(5);
-
-  //  mouse stuff, my own custom code. I found it from 
-  //  https://community.keyboard.io/t/do-you-use-the-keyboards-mouse-controls/501/23
-  MouseKeys.speed=24;
-  MouseKeys.speedDelay=3;
 }
 
 /** loop is the second of the standard Arduino sketch functions.
